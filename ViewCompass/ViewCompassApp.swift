ec6bf698400d56a872ba77f6cc1a322ee39e07f7
LubenZA <lubenivanchev@gmail.com>
2025-05-15 09:52:56 +0200

    Initial Commit

---
+++ ViewCompass/ViewCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewCompassApp.swift
+//  ViewCompass
+//
+//  Created by Luben Ivanchev on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct ViewCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewCompassTests/ViewCompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewCompassTests.swift
+//  ViewCompassTests
+//
+//  Created by Luben Ivanchev on 2025/05/15.
+//
+
+import Testing
+@testable import ViewCompass
+
+struct ViewCompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
